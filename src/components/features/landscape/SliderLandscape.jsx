bbc5ead9ec4c3f648a68167138057554d67d9fb3
Alice Cadillon <acadillon@gmail.com>
2025-12-01 14:02:35 +0100

    header style

--- src/components/features/landscape/SliderLandscape.jsx
+++ src/components/features/landscape/SliderLandscape.jsx
@@ -3,7 +3,7 @@
 const SliderLandscape = () => {
     return (
         <>
-            <div className="fixed top-0 right-0 w-screen h-screen bg-violet-100 md:w-[57.8vw] lg:w-[58vw] z-20 hover:[&+div]:opacity-60">
+            <div className="fixed top-0 right-0 w-screen h-screen bg-violet-100 opacity-60 md:w-[57.8vw] lg:w-[58vw] z-20 hover:[&+div]:opacity-60">
                 <a href="#landscape" className="absolute right-0">
                     <span>Image cover</span>
                 </a>
