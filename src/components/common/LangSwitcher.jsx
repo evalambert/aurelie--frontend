b97b785c9eb6aead22eca42c9bc71ee791c9d59e
evalambert <145194730+evalambert@users.noreply.github.com>
2025-12-03 13:51:27 +0100

    New LangSwith logic  with Navigate

---
+++ src/components/common/LangSwitcher.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { navigate } from "astro:transitions/client";
+
+const LangSwitcher = ({ currentLang, currentPath }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const otherLang = currentLang === "fr" ? "en" : "fr";
+
+  const cleanPath = currentPath.replace(/^\/(fr|en)/, "");
+
+  const handleLanguageChange = () => {
+    const newPath = `/${otherLang}${cleanPath}`;
+    navigate(newPath, { history: "push" });
+  };
+
+  return (
+    <button
+      onClick={handleLanguageChange}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      className="shrink-0 w-[35px]"
+    >
+      {isHovered ? `(${otherLang})` : `(${currentLang})`}
+    </button>
+  );
+};
+
+export default LangSwitcher;
