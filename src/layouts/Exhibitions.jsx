bb733120d3bce52973ac1db4be74e37c0067926e
evalambert <145194730+evalambert@users.noreply.github.com>
2025-12-09 14:25:01 +0100

    commit eva

--- src/layouts/Exhibitions.jsx
+++ src/layouts/Exhibitions.jsx
@@ -165,6 +165,9 @@ export default function Exhibitions({ data, lang }) {
                 )}
               </AnimatePresence>
             </li>
+            
+            
+
           ))}
       </ul>
     </div>
