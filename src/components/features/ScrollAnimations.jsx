77fc8fb8529776c67e41f32eeeda56df47ca92c2
Alice Cadillon <acadillon@gmail.com>
2025-12-10 20:21:39 +0100

    header links scroll trigger and hover function OK desktop and mobile

--- src/components/features/ScrollAnimations.jsx
+++ src/components/features/ScrollAnimations.jsx
@@ -2,38 +2,104 @@
 import { useEffect } from "react";
 
 export default function ScrollAnimations() {
-  useEffect(() => {
-    // import dynamique uniquement côté client
-    import("gsap").then((gsapModule) => {
-      const { gsap } = gsapModule;
-      import("gsap/ScrollTrigger").then((ScrollTriggerModule) => {
-        const { ScrollTrigger } = ScrollTriggerModule;
-        gsap.registerPlugin(ScrollTrigger);
+    useEffect(() => {
 
-        let sections = [...document.querySelectorAll("section[id]")];
 
-        // Créer un ScrollTrigger pour chaque section
-        // sections.forEach((section) => {
-        //   const sectionId = section.id;
-          
-        //   ScrollTrigger.create({
-        //     trigger: section,
-        //     start: "top 95px",
-        //     end: "bottom center",
-        //     markers: true, // Affiche les markers pour visualiser les triggers
-        //     onEnter: () => {
-        //       console.log(`Enter section: ${sectionId}`);
-        //     },
-        //     onEnterBack: () => {
-        //       console.log(`EnterBack section: ${sectionId}`);
-        //     },
-        //   });
-        // });
+        // GSAP
+        import("gsap").then((gsapModule) => {
+            const { gsap } = gsapModule;
+            import("gsap/ScrollTrigger").then((ScrollTriggerModule) => {
+                const { ScrollTrigger } = ScrollTriggerModule;
+                gsap.registerPlugin(ScrollTrigger);
 
-        console.log("ScrollAnimations initialized");
-      });
-    });
-  }, []);
+                let sections = [...document.querySelectorAll("section[id]")];
 
-  return null;
+                // Fonction pour mettre à jour le soulignement des liens de navigation
+                const updateActiveLink = (activeSectionId) => {
+                    const navLinks = document.querySelectorAll("header nav ul li a");
+
+                    navLinks.forEach((link) => {                        
+                        const href = link.getAttribute("href");
+                        const linkSectionId = href ? href.replace("#", "") : null;
+
+                        if (linkSectionId === activeSectionId) {
+                            link.classList.add("underline");
+                        } else {
+                            link.classList.remove("underline");
+                        }
+                    });
+                };
+
+                // Créer un ScrollTrigger pour chaque section
+                sections.forEach((section) => {
+                    const sectionId = section.id;
+
+                    ScrollTrigger.create({
+                        trigger: section,
+                        start: "top 97px",
+                        end: "bottom 97px",
+                        markers: true, // Affiche les markers pour visualiser les triggers
+                        onEnter: () => {
+                            console.log(`Enter section: ${sectionId}`);
+                            
+                            sections.forEach((s) => s.classList.remove("active"));
+                            section.classList.add("active");
+                            updateActiveLink(sectionId);
+
+                        },
+                        onEnterBack: () => {
+                            console.log(`EnterBack section: ${sectionId}`);
+
+                            sections.forEach((s) => s.classList.remove("active"));
+                            section.classList.add("active");
+                            updateActiveLink(sectionId);
+                        },
+                        onLeave: () => {
+                            if (sectionId == "exhibitions") {
+                                console.log(`Leave section: ${sectionId}`);
+                                updateActiveLink("atlas");
+                            }
+                        },
+                        onLeaveBack: () => {
+                            if (sectionId == "about") {
+                                console.log(`LeaveBack section: ${sectionId}`);
+                                const navLinks = document.querySelectorAll("header nav ul li a");
+                                navLinks.forEach((l) => l.classList.remove("underline"));
+                            }
+                        },
+                    });
+                });
+
+                console.log("ScrollAnimations initialized");
+            });
+        });
+
+
+        /* ----------------------------- HEADER HOVER LOGIC ----------------------------- */
+        const nav = document.querySelector("header nav"); ``
+        const navLinks = document.querySelectorAll("header nav ul li a");
+
+        nav.addEventListener("mouseenter", () => {
+            navLinks.forEach((l) => l.classList.remove("underline"));
+        });
+
+        nav.addEventListener("mouseleave", () => {
+            const activeSection = document.querySelector("section.active");
+            if (activeSection) {
+                const activeSectionId = activeSection.id;
+                navLinks.forEach((l) => {
+                    if (l.getAttribute("href") === `#${activeSectionId}`) {
+                        l.classList.add("underline");
+                    } 
+                });
+            }
+        });
+
+
+
+
+
+    }, []);
+
+    return null;
 }
