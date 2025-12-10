880ded7ad8a0e5091d36c6e5f4c56d8142a13115
Alice Cadillon <acadillon@gmail.com>
2025-12-10 12:21:11 +0100

    preview exhibition pannel desktop ok for exhibition images

--- src/components/features/exhibitions/ImagesExhibition.jsx
+++ src/components/features/exhibitions/ImagesExhibition.jsx
@@ -1,25 +1,49 @@
-//components/features/exhibitions/ImageExhibition.jsx
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { getResponsiveImageUrl } from "../../../assets/scripts/libs/getImageUrl";
+import { previewStore } from "../../../stores/previewStore";
 
 export default function ImagesExhibition({ item }) {
+
   return (
-    <div className="exhibition--images flex flex-col gap-[10px] md:pl-[55px]">
+    <div className="exhibition--images flex flex-col md:pl-[55px]">
       {item.exhibitionView?.map((img) => {
         const isLandscape = img.width > img.height;
+        const [imageUrl, setImageUrl] = useState(null);
+
+        useEffect(() => {
+          const updateImage = () => {
+            const width = window.innerWidth;
+            const url = getResponsiveImageUrl(img, "card", width);
+            setImageUrl(url);
+          };
+          updateImage();
+          window.addEventListener("resize", updateImage);
+          return () => window.removeEventListener("resize", updateImage);
+        }, [img]);
 
         return (
           <div key={img.id} className="w-full flex justify-start">
-            <img
-              src={img.url}
-              alt={img.alternativeText || img.name}
-              width={img.width}
-              height={img.height}
-              loading="lazy"
-              className={
-                "filter grayscale opacity-50 w-full h-auto " +
-                (isLandscape ? "md:w-[70%]" : "md:w-[50%]")
-              }
-            />
+            {imageUrl && (
+              <img
+                src={imageUrl}
+                alt={img.alternativeText || img.name}
+                width={img.width}
+                height={img.height}
+                loading="lazy"
+                onPointerEnter={() => {
+                  previewStore.setHoverImage(img.formats?.large?.url || img.url);
+                  console.log("image enter");
+                }
+                }
+                onPointerLeave={() =>
+                  previewStore.clearHover()
+                }
+                className={
+                  "filter grayscale opacity-50 w-full h-auto pb-[10px] " +
+                  (isLandscape ? "md:w-[70%]" : "md:w-[50%]")
+                }
+              />
+            )}
           </div>
         );
       })}
--- src/components/features/landscape/SliderLandscape.jsx
+++ src/components/features/landscape/SliderLandscape.jsx
@@ -146,7 +146,7 @@ const SliderLandscape = ({ slider, mode, isMobile, onMouseLeave }) => {
           draggable="false"
           className={` max-md:pb-[10px] max-md:pl-[10px] md:-mt-[17px] md:pointer-events-none max-h-[calc(100vh-(var(--spacing-y-body)*2))] ${
             isLandscape
-              ? "max-w-[calc(100vw-var(--spacing-x-body))] md:max-w-[calc(57.8vw-var(--spacing-x-body))] lg:max-w-[calc(58vw-var(--spacing-x-body))]"
+              ? "max-w-[calc(100vw-var(--spacing-x-body))] md:max-w-[calc(57.8vw-10px)] lg:max-w-[calc(58vw-10px)]"
               : "max-w-[80vw] pl-[10px]"
           }`}
           alt=""
