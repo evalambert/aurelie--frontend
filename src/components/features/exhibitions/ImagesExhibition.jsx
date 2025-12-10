bad7bc92fe7052c2e159f591a43cf52e40d3044a
evalambert <145194730+evalambert@users.noreply.github.com>
2025-12-10 20:11:45 +0100

    version clean

--- src/components/features/exhibitions/ImagesExhibition.jsx
+++ src/components/features/exhibitions/ImagesExhibition.jsx
@@ -1,52 +1,12 @@
-import { useState, useEffect } from "react";
-import { getResponsiveImageUrl } from "../../../assets/scripts/libs/getImageUrl";
-import { previewStore } from "../../../stores/previewStore";
+// src/components/features/exhibitions/ImagesExhibition.jsx
+import ImageExhibitionItem from "./ImageExhibitionItem";
 
 export default function ImagesExhibition({ item }) {
-
   return (
     <div className="exhibition--images flex flex-col md:pl-[55px]">
-      {item.exhibitionView?.map((img) => {
-        const isLandscape = img.width > img.height;
-        const [imageUrl, setImageUrl] = useState(null);
-
-        useEffect(() => {
-          const updateImage = () => {
-            const width = window.innerWidth;
-            const url = getResponsiveImageUrl(img, "card", width);
-            setImageUrl(url);
-          };
-          updateImage();
-          window.addEventListener("resize", updateImage);
-          return () => window.removeEventListener("resize", updateImage);
-        }, [img]);
-
-        return (
-          <div key={img.id} className="w-full flex justify-start">
-            {imageUrl && (
-              <img
-                src={imageUrl}
-                alt={img.alternativeText || img.name}
-                width={img.width}
-                height={img.height}
-                loading="lazy"
-                onPointerEnter={() => {
-                  previewStore.setHoverImage(img.formats?.large?.url || img.url);
-                  console.log("image enter");
-                }
-                }
-                onPointerLeave={() =>
-                  previewStore.clearHover()
-                }
-                className={
-                  "filter grayscale opacity-50 w-full h-auto pb-[10px] " +
-                  (isLandscape ? "md:w-[70%]" : "md:w-[50%]")
-                }
-              />
-            )}
-          </div>
-        );
-      })}
+      {item.exhibitionView?.map((img) => (
+        <ImageExhibitionItem key={img.id} img={img} />
+      ))}
     </div>
   );
 }
