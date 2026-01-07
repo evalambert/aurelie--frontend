036e3e5b0698242f5ed38aafdf527683a56a48f8
Alice Cadillon <acadillon@gmail.com>
2026-01-07 11:34:31 +0100

    image format expo movile

--- src/assets/scripts/libs/getImageUrl.js
+++ src/assets/scripts/libs/getImageUrl.js
@@ -7,6 +7,10 @@ export const getResponsiveImageUrl = (image, context, width) => {
       if (width >= 1024) return image.formats.medium?.url || image.url
       if (width >= 768) return image.formats.medium?.url || image.url
       return image.formats.small?.url || image.url
+    case 'medium':
+      if (width >= 1024) return image.formats.medium?.url || image.url
+      if (width >= 768) return image.formats.medium?.url || image.url
+      return image.formats.large?.url || image.url
     case 'lightbox':
       if (width >= 1024) return image.formats.xlarge?.url || image.url
       if (width >= 768) return image.formats.large?.url || image.url
