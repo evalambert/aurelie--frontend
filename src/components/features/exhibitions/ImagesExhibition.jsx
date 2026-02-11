571b0d98d479502a6af0d9f550120cf324e5fa6f
Alice Cadillon <acadillon@gmail.com>
2026-02-11 14:23:14 +0100

    section exxhibition ok

--- src/components/features/exhibitions/ImagesExhibition.jsx
+++ src/components/features/exhibitions/ImagesExhibition.jsx
@@ -7,7 +7,7 @@ import ImageExhibitionItem from "./ImageExhibitionItem";
 export default function ImagesExhibition({ item }) {
   
   return (
-    <div className="exhibition--images flex flex-col md:pl-[55px]">
+    <div className="exhibition--images flex flex-col md:pl-[55px] gap-[10px]">
       {item.exhibitionView?.map((img) => (
         <ImageExhibitionItem key={img.id} img={img} />
       ))}
